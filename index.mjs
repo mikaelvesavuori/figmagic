0fe7d5092c4fbe079700a12770ab61b5ce2cf0ec
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-02-22 23:52:13 +0100

    Update

--- index.mjs
+++ index.mjs
@@ -13,6 +13,7 @@ import { writeTokens } from './bin/functions/writeTokens.mjs';
 import { errorGetData } from './bin/meta/errors.mjs';
 
 (async () => {
+  // Setup
   const [, , ...CLI_ARGS] = process.argv;
   const USER_CONFIG_PATH = `${process.cwd()}/.figmagicrc`;
   const CONFIG = await createConfiguration(USER_CONFIG_PATH, ...CLI_ARGS);
@@ -21,19 +22,18 @@ import { errorGetData } from './bin/meta/errors.mjs';
   // Attempt to get data
   const DATA = await getFromApi(token, url, outputFolderBaseFile, outputFileName);
 
-  // If there's data and all is OK, start processing tokens
-  if (DATA && DATA.status !== 403) {
-    // Remove old folders
-    await trash([`./${outputFolderTokens}`]);
-    await trash([`./${outputFolderBaseFile}`]);
+  // If there's no data or something went funky, eject
+  if (!DATA || DATA.status === 403) throw new Error(errorGetData);
 
-    // Add new folders
-    createFolder(outputFolderTokens);
-    createFolder(outputFolderBaseFile);
+  // Remove old folders
+  await trash([`./${outputFolderTokens}`]);
+  await trash([`./${outputFolderBaseFile}`]);
 
-    const TOKENS = createPage(DATA.document.children);
-    writeTokens(TOKENS.children, CONFIG);
-  } else {
-    console.error(errorGetData);
-  }
+  // Add new folders
+  createFolder(outputFolderTokens);
+  createFolder(outputFolderBaseFile);
+
+  // Process tokens
+  const TOKENS = createPage(DATA.document.children);
+  writeTokens(TOKENS.children, CONFIG);
 })();
