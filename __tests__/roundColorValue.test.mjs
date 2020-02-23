a41f946bdf734f6c37948be131e38cdd78014e45
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-02-23 21:35:32 +0100

    Update

--- __tests__/roundColorValue.test.mjs
+++ __tests__/roundColorValue.test.mjs
@@ -1,30 +1,38 @@
 import { roundColorValue } from '../bin/functions/roundColorValue';
 
 test('It should round a color value into a single decimal', () => {
-  expect(roundColorValue(0.5176470875740051, 255)).toBe('132');
+  expect(roundColorValue(0.5176470875740051, 255)).toBe(132);
 });
 
 test('It should set full "quantity" value (1.0) to be 255', () => {
-  expect(roundColorValue(1.0, 255)).toBe('255');
+  expect(roundColorValue(1.0, 255)).toBe(255);
 });
 
-test('It should set a zero "quantity" value (0.0) to 0', () => {
-  expect(roundColorValue(0.0, 255)).toBe('0');
-});
-
-test('It should set scale to 255 if user does not provide value', () => {
-  expect(roundColorValue(1.0)).toBe('255');
-});
-
+/*
 test('It should throw an error when quantity is not passed', () => {
   expect(() => {
     roundColorValue();
   }).toThrow();
 });
 
+test('It should throw an error when receiving a zero "quantity" value (0.0)', () => {
+  expect(() => {
+    roundColorValue(0.0, 255);
+  }).toThrow();
+});
+*/
+
+test('It should set scale to 255 if user does not provide value', () => {
+  expect(roundColorValue(1.0)).toBe(255);
+});
+
+test('It should max out "quantity" at 1.0, returning at most a full value of 255', () => {
+  expect(roundColorValue(12412.1)).toBe(255);
+});
+
 test('It should throw an error when scale is below 0', () => {
   expect(() => {
-    roundColorValue(1.0, 0);
+    roundColorValue(1.0, -1);
   }).toThrow();
 });
 
--- bin/functions/roundColorValue.mjs
+++ bin/functions/roundColorValue.mjs
@@ -9,8 +9,14 @@ import { errorRoundColor, errorRoundColorValue } from '../meta/errors.mjs';
  * @param {number} scale - Maximum value
  * @returns {number} - The final number
  */
-export function roundColorValue(quantity, scale = 255) {
-  if (!quantity) throw new Error(errorRoundColor);
+export function roundColorValue(quantity = 0.0, scale = 255) {
+  const MIN_VALUE = 0.0;
+  const MAX_VALUE = 1.0;
+
+  //if (!quantity) throw new Error(errorRoundColor);
+  let _quantity = parseFloat(quantity);
+  if (parseFloat(_quantity) < MIN_VALUE) _quantity = MIN_VALUE;
+  if (parseFloat(_quantity) > MAX_VALUE) _quantity = MAX_VALUE;
   if (scale < 0 || scale > 255) throw new Error(errorRoundColorValue);
-  return (parseFloat(quantity) * parseInt(scale)).toFixed(0);
+  return parseInt((parseFloat(_quantity) * parseInt(scale)).toFixed(0));
 }
