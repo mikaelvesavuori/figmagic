96996de2e7cd08ad4294d4e2394e24e3b8e58779
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-02-24 21:43:29 +0100

    feat(attempt to fix install issue): try to fix install issue with NPM

--- index.mjs
+++ index.mjs
@@ -33,7 +33,7 @@ import { errorGetData } from './bin/meta/errors.mjs';
   await createFolder(outputFolderBaseFile);
 
   // Write base Figma JSON
-  await writeFile(JSON.stringify(DATA), outputFolderBaseFile, outputFileName);
+  writeFile(JSON.stringify(DATA), outputFolderBaseFile, outputFileName);
 
   // Process tokens
   const TOKENS = createPage(DATA.document.children);
