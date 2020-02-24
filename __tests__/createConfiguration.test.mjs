d15f1f0ca89e2362ca03c92176c966afe507dcff
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-02-24 20:44:54 +0100

    Update

--- __tests__/createConfiguration.test.mjs
+++ __tests__/createConfiguration.test.mjs
@@ -1,7 +1,7 @@
 import { createConfiguration } from '../bin/functions/createConfiguration';
 
 test('It should return a valid merged configuration if given a path to an RC file and a set of CLI arguments', async () => {
-  const USER_CONFIG_PATH = `${process.cwd()}/.figmagicrc`;
+  const USER_CONFIG_PATH = `${process.cwd()}/testdata/figmagicrc`;
   const CLI_ARGS = ['-t', 'asdf1234'];
 
   expect(await createConfiguration(USER_CONFIG_PATH, ...CLI_ARGS)).toEqual(
@@ -13,8 +13,6 @@ test('It should return a valid merged configuration if given a path to an RC fil
       outputFolderTokens: 'tokens',
       outputTokenFormat: 'mjs',
       spacingUnit: 'rem',
-      token: 'asdf1234',
-      url: null,
       usePostscriptFontNames: false
     })
   );
