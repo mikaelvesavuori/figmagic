c542a7349b63e21bda5caf1bd63bf6b14f0fdf9d
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-03-17 20:42:44 +0100

    test(attempt to fix weird async thing in test): attempt to fix weird async thing in test
    Attempt to fix weird async thing in test

--- __tests__/writeFile.test.mjs
+++ __tests__/writeFile.test.mjs
@@ -9,11 +9,11 @@ test('It should throw an error if no parameter is provided', async () => {
 test('It can write to a file if provided input', async () => {
   const FILE = '__test-writefile1.txt';
   await writeFile(JSON.stringify({ something: 1234 }), './', FILE);
-  trash(`__test-writefile*`);
+  await trash(`__test-writefile*`);
 });
 
 test('It can write a token to a file if provided input', async () => {
   const FILE = '__test-writefile2.txt';
   await writeFile(JSON.stringify({ something: 1234 }), './', FILE, true);
-  trash(`__test-writefile*`);
+  await trash(`__test-writefile*`);
 });
