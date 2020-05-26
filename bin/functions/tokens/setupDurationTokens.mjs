f68fbc66f5e96bdfe5a2b2c2d24a27438e7f2fc4
Stanko <stanko.tadic@gmail.com>
2020-05-26 11:53:41 +0200

    Added 'durations' token type

---
+++ bin/functions/tokens/setupDurationTokens.mjs
@@ -0,0 +1,37 @@
+import { camelize } from '../helpers/camelize.mjs';
+import { formatName } from '../helpers/formatName.mjs';
+
+import {
+  errorSetupDurationTokensNoFrame,
+  errorSetupDurationTokensNoChildren,
+  errorSetupDurationTokensMissingProps
+} from '../../meta/errors.mjs';
+
+/**
+ * Places all Figma durations into a clean object
+ *
+ * @exports
+ * @function
+ * @param {object} durationFrame - The durations frame from Figma
+ * @returns {object} - Returns an object with all the Z indices
+ * @throws {errorSetupDurationTokensNoFrame} - When there is no provided Figma frame
+ * @throws {errorSetupDurationTokensNoChildren} - When no children in Figma frame
+ * @throws {errorSetupDurationTokensMissingProps} - When missing required props in children
+ */
+export function setupDurationTokens(durationFrame) {
+  if (!durationFrame) throw new Error(errorSetupDurationTokensNoFrame);
+  if (!durationFrame.children) throw new Error(errorSetupDurationTokensNoChildren);
+
+  let durationObject = {};
+
+  durationFrame.children.forEach((type) => {
+    if (!type.name || !type.characters) throw new Error(errorSetupDurationTokensMissingProps);
+
+    let name = camelize(type.name);
+    name = formatName(name);
+
+    durationObject[name] = parseFloat(type.characters);
+  });
+
+  return durationObject;
+}
