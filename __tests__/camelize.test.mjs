322a3af672b307f5ccda1afddc49b7a616aacb2d
Stanko <stanko.tadic@gmail.com>
2020-06-04 13:58:40 +0200

    Fixed camelize function and added more test cases

--- __tests__/camelize.test.mjs
+++ __tests__/camelize.test.mjs
@@ -8,12 +8,32 @@ test('It should lower-case the string', () => {
   expect(camelize('ASDF')).toBe('asdf');
 });
 
-test('It should trim trailing whitespace', () => {
-  expect(camelize('text       ')).toBe('text');
+test('It should trim whitespace', () => {
+  expect(camelize('     text       ')).toBe('text');
 });
 
 test('It should throw an error if no parameter is provided', () => {
   expect(() => {
     camelize();
   }).toThrow();
+});
+
+test('It should not change already camelcased word', () => {
+  expect(camelize('camelCaseWord')).toBe('camelCaseWord');
+});
+
+test('It should convert snake case', () => {
+  expect(camelize('snake_case_word')).toBe('snakeCaseWord');
+});
+
+test('It should convert kebab case', () => {
+  expect(camelize('kebab-case-word')).toBe('kebabCaseWord');
+});
+
+test('It should consider mutiple spaces as a single space', () => {
+  expect(camelize('hello               world')).toBe('helloWorld');
+});
+
+test('It should consider all characters that are not letter nor digit as a space', () => {
+  expect(camelize('hello }[";-_world')).toBe('helloWorld');
 });
