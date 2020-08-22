fce295f3d1f05c43864149dfb86238d970dfb986
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2020-08-22 10:52:23 +0200

    Update

--- bin/usecases/createGraphics.ts
+++ bin/usecases/createGraphics.ts
@@ -1,12 +1,12 @@
 import { FigmaData } from '../../entities/FigmaData/FigmaData';
 import { Config } from '../contracts/config/Config';
 
-import { doSyncElements } from '../main/doSyncElements';
+import { doSyncGraphics } from '../main/doSyncGraphics';
 
 export async function createGraphics(
   config: Config,
   data: FigmaData,
   outputFolderElements: string
 ) {
-  return await doSyncElements(config, data, outputFolderElements);
+  return await doSyncGraphics(config, data, outputFolderElements);
 }
