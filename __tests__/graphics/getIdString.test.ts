ffebe92fd0fab8578d019c2ba87a9b4eb3834f12
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2020-08-31 09:19:26 +0200

    Update

--- __tests__/graphics/getIdString.test.ts
+++ __tests__/graphics/getIdString.test.ts
@@ -1,16 +1,16 @@
 import { getIdString } from '../../bin/app/process/graphics/getIdString';
 
+import { graphicsIds } from '../../testdata/graphics/getGraphics';
+
 describe('Failure cases', () => {
-  test('It should throw an error if no parameter is provided', async () => {
+  test('It should throw an error if no parameter is provided', () => {
     // @ts-ignore
-    expect(getIdString()).toThrow();
+    expect(() => getIdString()).toThrow();
   });
 });
 
 describe('Success cases', () => {
-  /*
-  test('It should normalize a percent unit to unitless, when given a width value, current unit string, and a conversion type as float', () => {
-    expect(getFileList(146.484375, 'percent', 'unitless')).toBe('1.46484375');
+  test('It should correctly product a string of IDs from an array of IDs', () => {
+    expect(getIdString(graphicsIds)).toBe('2710:7,2710:5');
   });
-  */
 });
