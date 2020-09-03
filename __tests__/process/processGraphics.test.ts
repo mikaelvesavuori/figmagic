338ec202de8967119a5c1ba449a2c227bd0e67a4
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2020-09-03 11:21:39 +0200

    Update

--- __tests__/process/processGraphics.test.ts
+++ __tests__/process/processGraphics.test.ts
@@ -3,51 +3,12 @@ import dotenv from 'dotenv';
 import { Config } from '../../bin/entities/Config/Config';
 
 import { processGraphics } from '../../bin/app/process/processGraphics';
-import { getIds } from '../../bin/app/process/graphics/getIds';
-import { getIdString } from '../../bin/app/process/graphics/getIdString';
-import { getFileList } from '../../bin/app/process/graphics/getFileList';
 
-import { getFromApi } from '../../bin/frameworks/network/getFromApi';
-
-//import { graphicsFrame } from '../../testdata/frames/graphicsFrame';
 import { graphicsPage } from '../../testdata/graphicsPage';
 
 dotenv.config();
 
-// TODO: Test loc 27-37
-
-describe('Failure cases', () => {
-  test('It should throw an error if no argument is provided', async () => {
-    // @ts-ignore
-    await expect(processGraphics()).rejects.toThrow();
-  });
-
-  test('It should throw when missing arguments', () => {
-    // @ts-ignore
-    expect(() => getFileList()).toThrow();
-  });
-
-  test('It should throw when missing argument', () => {
-    // @ts-ignore
-    expect(() => getIds()).toThrow();
-  });
-
-  test('It should throw when missing items in argument', () => {
-    // @ts-ignore
-    expect(() => getIds({})).toThrow();
-  });
-
-  test('It should throw when missing ids', () => {
-    // @ts-ignore
-    expect(() => getIdString()).toThrow();
-  });
-
-  test('It should throw an error when receiving invalid token and/or URL', async () => {
-    expect(await getFromApi('asdf', 'asdf')).toEqual(
-      expect.objectContaining({ err: 'Invalid token', status: 403 })
-    );
-  });
-});
+// Re-ordered success and failure cases because the "normal" order will leak variables or something, causing tests to fail
 
 describe('Success cases', () => {
   test('It should exit correctly after having processed valid input', async () => {
@@ -65,9 +26,51 @@ describe('Success cases', () => {
       } as Config)
     ).resolves.toEqual(expect.objectContaining({}));
   });
+});
 
-  test('It should find valid data (assuming the base document ID to be "0:0") when passed valid token and URL', async () => {
-    const DATA = await getFromApi(process.env.FIGMA_TOKEN, process.env.FIGMA_URL);
-    expect(DATA.document.id).toEqual('0:0');
+describe('Failure cases', () => {
+  test('It should throw an error if no argument is provided', async () => {
+    // @ts-ignore
+    await expect(processGraphics()).rejects.toThrow();
+  });
+
+  test('It should throw an error if missing token', async () => {
+    const xxx = graphicsPage;
+    xxx[0].children = null;
+
+    await expect(
+      // @ts-ignore
+      processGraphics(xxx, {
+        outputFileName: 'figma.json',
+        outputFolderBaseFile: '.figmagic',
+        outputFolderTokens: 'tokens',
+        outputFolderGraphics: 'graphics',
+        outputFormatGraphics: 'svg',
+        outputScaleGraphics: 1,
+        token: null,
+        url: process.env.FIGMA_URL
+      } as Config)
+    ).rejects.toThrow();
+  });
+
+  test('It should throw an error if missing children in graphics page', async () => {
+    const data = graphicsPage;
+    data[0].children = null;
+
+    await expect(
+      // @ts-ignore
+      processGraphics(data, {
+        outputFileName: 'figma.json',
+        outputFolderBaseFile: '.figmagic',
+        outputFolderTokens: 'tokens',
+        outputFolderGraphics: 'graphics',
+        outputFormatGraphics: 'svg',
+        outputScaleGraphics: 1,
+        token: process.env.FIGMA_TOKEN,
+        url: process.env.FIGMA_URL
+      } as Config)
+    ).rejects.toThrow();
   });
 });
+
+// 27,36-37
