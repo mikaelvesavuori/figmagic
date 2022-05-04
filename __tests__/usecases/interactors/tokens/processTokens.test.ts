fc236a4734d8b7f14b3c1aeb64b9f8c477685808
mikaelvesavuori <mikaelvesavuori@gmail.com>
2022-05-04 22:00:56 +0200

    fix(): issue 154, remove snyk, update dependencies

--- __tests__/usecases/interactors/tokens/processTokens.test.ts
+++ __tests__/usecases/interactors/tokens/processTokens.test.ts
@@ -327,7 +327,7 @@ describe('Success cases', () => {
         {
           file: {
             deep: '3px 3px 3px rgba(196, 196, 196, 0.75)',
-            deepMulti: '0px 0px 4px rgba(0, 0, 0, 0.17), 0px 0px 20px rgba(0, 0, 0, 0.1)',
+            deepMulti: '0px 4px 4px rgba(0, 0, 0, 0.17), 0px 4px 20px rgba(0, 0, 0, 0.1)',
             medium: '0px 0px 5px rgba(0, 0, 0, 0.5)',
             soft: '0px 0px 5px rgba(196, 196, 196, 1)'
           },
