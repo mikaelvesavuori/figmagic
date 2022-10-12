a34d8279bfb774051871c65c0021a918b9e82750
Paulo Henrique <paulo.scape@gmail.com>
2022-10-11 17:08:48 -0700

    adding test sample for useLiteralFontFamilies

--- testdata/frames/fontFrame.ts
+++ testdata/frames/fontFrame.ts
@@ -139,6 +139,145 @@ export const fontFrame: Frame = {
   effects: []
 };
 
+export const literalFontFamilyFrame: Frame = {
+  id: '1:52',
+  name: 'Font Families',
+  type: 'FRAME',
+  blendMode: 'PASS_THROUGH',
+  children: [
+    {
+      id: '1:53',
+      name: 'familySans',
+      type: 'TEXT',
+      blendMode: 'PASS_THROUGH',
+      absoluteBoundingBox: { x: 587, y: 250, width: 144, height: 19 },
+      constraints: { vertical: 'TOP', horizontal: 'LEFT' },
+      fills: [
+        {
+          blendMode: 'NORMAL',
+          type: 'SOLID',
+          color: { r: 0, g: 0, b: 0, a: 1 }
+        }
+      ],
+      strokes: [],
+      strokeWeight: 0,
+      strokeAlign: 'OUTSIDE',
+      effects: [],
+      characters: 'Roboto, BlinkMacSystemFont, Helvetica, Arial, sans-serif',
+      style: {
+        fontFamily: 'Helvetica Neue',
+        fontPostScriptName: 'HelveticaNeue',
+        fontWeight: 400,
+        paragraphSpacing: 1,
+        fontSize: 16,
+        textAlignHorizontal: 'LEFT',
+        textAlignVertical: 'TOP',
+        letterSpacing: 0,
+        lineHeightPx: 18.75,
+        lineHeightPercent: 100,
+        lineHeightUnit: 'INTRINSIC_%'
+      },
+      characterStyleOverrides: [],
+      styleOverrideTable: {},
+      styles: { text: '1:91' }
+    },
+    {
+      id: '1:54',
+      name: 'familySerif',
+      type: 'TEXT',
+      blendMode: 'PASS_THROUGH',
+      absoluteBoundingBox: { x: 442, y: 250, width: 116, height: 19 },
+      constraints: { vertical: 'TOP', horizontal: 'LEFT' },
+      fills: [
+        {
+          blendMode: 'NORMAL',
+          type: 'SOLID',
+          color: { r: 0, g: 0, b: 0, a: 1 }
+        }
+      ],
+      strokes: [],
+      strokeWeight: 0,
+      strokeAlign: 'OUTSIDE',
+      effects: [],
+      characters: 'Georgia, Times, Times New Roman, serif',
+      style: {
+        fontFamily: 'Georgia',
+        fontPostScriptName: 'Georgia',
+        fontWeight: 400,
+        paragraphSpacing: 1,
+        fontSize: 16,
+        textAlignHorizontal: 'LEFT',
+        textAlignVertical: 'TOP',
+        letterSpacing: 0,
+        lineHeightPx: 18.75,
+        lineHeightPercent: 100,
+        lineHeightUnit: 'INTRINSIC_%'
+      },
+      characterStyleOverrides: [],
+      styleOverrideTable: {},
+      styles: { text: '1:90' }
+    },
+    {
+      id: '1:55',
+      name: 'familyCode',
+      type: 'TEXT',
+      blendMode: 'PASS_THROUGH',
+      absoluteBoundingBox: { x: 310, y: 250, width: 92, height: 19 },
+      constraints: { vertical: 'TOP', horizontal: 'LEFT' },
+      fills: [
+        {
+          blendMode: 'NORMAL',
+          type: 'SOLID',
+          color: { r: 0, g: 0, b: 0, a: 1 }
+        }
+      ],
+      strokes: [],
+      strokeWeight: 0,
+      strokeAlign: 'OUTSIDE',
+      effects: [],
+      characters: "'Roboto Mono', Menlo, Courier, mc",
+      style: {
+        fontFamily: 'Roboto Mono',
+        fontPostScriptName: 'RobotoMono',
+        fontWeight: 400,
+        paragraphSpacing: 1,
+        fontSize: 16,
+        textAlignHorizontal: 'LEFT',
+        textAlignVertical: 'TOP',
+        letterSpacing: 0,
+        lineHeightPx: 18.75,
+        lineHeightPercent: 100,
+        lineHeightUnit: 'INTRINSIC_%'
+      },
+      characterStyleOverrides: [],
+      styleOverrideTable: {},
+      styles: { text: '1:89' }
+    }
+  ],
+  absoluteBoundingBox: { x: 270, y: 210, width: 504, height: 99 },
+  constraints: { vertical: 'TOP', horizontal: 'LEFT' },
+  clipsContent: true,
+  background: [
+    {
+      blendMode: 'NORMAL',
+      type: 'SOLID',
+      color: { r: 1, g: 1, b: 1, a: 1 }
+    }
+  ],
+  fills: [
+    {
+      blendMode: 'NORMAL',
+      type: 'SOLID',
+      color: { r: 1, g: 1, b: 1, a: 1 }
+    }
+  ],
+  strokes: [],
+  strokeWeight: 1,
+  strokeAlign: 'INSIDE',
+  backgroundColor: { r: 1, g: 1, b: 1, a: 1 },
+  effects: []
+};
+
 export const fontFrameInvalid = {
   children: [
     {
