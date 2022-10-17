4ea6098f658a9c38612ec48bf46fb2efbdca53e1
Paulo Henrique <paulo.scape@gmail.com>
2022-10-17 01:01:19 -0700

    updating makeColorTokens to read one depth frame

--- bin/entities/Token/logic/makeColorTokens.ts
+++ bin/entities/Token/logic/makeColorTokens.ts
@@ -1,8 +1,6 @@
 import { FRAME as Frame } from '../../../contracts/Figma';
-import { ColorTokens } from '../../../contracts/Tokens';
+import { ColorTokens, Tokens } from '../../../contracts/Tokens';
 import { OutputFormatColors } from '../../../contracts/Config';
-import { Color } from '../../../contracts/Parsing';
-
 import { sanitizeString } from '../../../frameworks/string/sanitizeString';
 import { createSolidColorString } from '../../../frameworks/string/createSolidColorString';
 import { createLinearGradientString } from '../../../frameworks/string/createLinearGradientString';
@@ -19,28 +17,37 @@ import {
 export function makeColorTokens(
   colorFrame: Frame,
   outputFormatColors: OutputFormatColors,
-  camelizeTokenNames?: boolean
+  camelizeTokenNames?: boolean,
+  depth = 0
 ): ColorTokens {
   if (!colorFrame) throw Error(ErrorMakeColorTokensNoFrame);
   if (!colorFrame.children) throw Error(ErrorMakeColorTokensNoChildren);
 
-  const colors: Record<string, string> = {};
+  const colors: ColorTokens = {};
   const tokens = colorFrame.children.reverse();
-  tokens.forEach((item: Frame) =>
-    makeColorToken(item, colors, outputFormatColors, camelizeTokenNames)
-  );
+  tokens.forEach((item: Frame) => {
+    if (depth === 0 && item.type === 'FRAME') {
+      colors[item.name] = makeColorTokens(
+        item,
+        outputFormatColors,
+        camelizeTokenNames,
+        depth + 1
+      ) as Tokens;
+    } else {
+      makeColorToken(item, colors, outputFormatColors, camelizeTokenNames);
+    }
+  });
 
-  return colors as ColorTokens;
+  return colors;
 }
 
 function makeColorToken(
   item: Frame,
-  colors: Color,
+  colors: ColorTokens,
   outputFormatColors: OutputFormatColors,
   camelizeTokenNames?: boolean
-) {
-  // @ts-ignore
-  if (!item.fills || item.fills.length === 0) return null;
+): void {
+  if (!item.fills || item.fills.length === 0) return;
 
   const name = sanitizeString(item.name, camelizeTokenNames);
   const fills = item.fills[0];
