62f4135598af4d8568dc7c456e02d3fda3f7c5c7
Paulo Henrique <paulo.scape@gmail.com>
2022-10-26 01:25:58 -0700

    build

---
+++ build/bin/usecases/interactors/graphics/optimizeSVGGraphics.d.ts
@@ -0,0 +1,3 @@
+import { Config } from '../../../contracts/Config';
+import { FileList } from '../../../contracts/Files';
+export declare function optimizeSVGGraphics(fileList: FileList[], config: Config): Promise<void>;
