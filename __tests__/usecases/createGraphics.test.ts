5e5c4df4c38f5a3d7be707951a417ae5a8e0f102
Paulo Henrique <paulo.scape@gmail.com>
2022-10-26 01:19:05 -0700

    updating createGraphics test

--- __tests__/usecases/createGraphics.test.ts
+++ __tests__/usecases/createGraphics.test.ts
@@ -7,42 +7,64 @@ import { loadEnv } from '../../bin/frameworks/system/loadEnv';
 
 import { testConfig } from '../../testdata/testConfig';
 import figmaTestResponse from '../../testdata/figma.json';
+import { optimizeSVGGraphics } from '../../bin/usecases/interactors/graphics/optimizeSVGGraphics';
 
 loadEnv();
 
 const FIGMA_TOKEN = process.env.IS_MOCK_ENABLED ? 'mocked' : process.env.FIGMA_TOKEN;
 const FIGMA_URL = process.env.IS_MOCK_ENABLED ? 'mocked' : process.env.FIGMA_URL;
 
-describe('Failure cases', () => {
-  test('It should throw an error if no argument is provided', async () => {
-    // @ts-ignore
-    await expect(createGraphics()).rejects.toThrowError();
-  });
+jest.mock('../../bin/usecases/interactors/graphics/optimizeSVGGraphics', () => ({
+  optimizeSVGGraphics: jest.fn()
+}));
 
-  test('It should throw an error if misconfigured (missing token and URL)', async () => {
-    const CONFIG = testConfig;
-    const DATA = figmaTestResponse;
-    CONFIG.outputFolderGraphics = '__test-graphics__';
-    // @ts-ignore
-    await expect(createGraphics(CONFIG, DATA)).rejects.toThrowError();
-    await trash(CONFIG.outputFolderGraphics);
-  });
+afterEach(() => {
+  jest.resetAllMocks();
 });
 
-describe('Success cases', () => {
-  test('It should write tokens given a valid configuration, valid data and an output folder', async () => {
-    const CONFIG = testConfig;
-    // @ts-ignore
-    CONFIG.token = FIGMA_TOKEN;
-    // @ts-ignore
-    CONFIG.url = FIGMA_URL;
-    const DATA = figmaTestResponse;
-    CONFIG.outputFolderGraphics = '__test-graphics-success__';
+describe('createGraphics test', () => {
+  const DATA = figmaTestResponse;
 
-    // @ts-ignore
-    await createGraphics(CONFIG, DATA);
-    const FILE_EXISTS = fs.existsSync(CONFIG.outputFolderGraphics);
-    expect(FILE_EXISTS).toBeTruthy();
-    await trash(CONFIG.outputFolderGraphics);
+  describe('Failure cases', () => {
+    test('It should throw an error if no argument is provided', async () => {
+      // @ts-ignore
+      await expect(createGraphics()).rejects.toThrowError();
+    });
+
+    test('It should throw an error if misconfigured (missing token and URL)', async () => {
+      const CONFIG = { ...testConfig, outputFolderGraphics: '__test-graphics__' };
+      // @ts-ignore
+      await expect(createGraphics(CONFIG, DATA)).rejects.toThrowError();
+      await trash(CONFIG.outputFolderGraphics);
+    });
+  });
+
+  describe('Success cases', () => {
+    const CONFIG = {
+      ...testConfig,
+      token: FIGMA_TOKEN,
+      url: FIGMA_URL,
+      outputFolderGraphics: '__test-graphics-success__'
+    };
+
+    test('It should write tokens given a valid configuration, valid data and an output folder', async () => {
+      // @ts-ignore
+      await createGraphics(CONFIG, DATA);
+      const FILE_EXISTS = fs.existsSync(CONFIG.outputFolderGraphics);
+      expect(FILE_EXISTS).toBeTruthy();
+
+      await trash(CONFIG.outputFolderGraphics);
+      expect(optimizeSVGGraphics).toHaveBeenCalled();
+    });
+    test('It should not call optimizeSVGGraphics() if output format is not SVG', async () => {
+      CONFIG.outputFormatGraphics = 'png';
+
+      // @ts-ignore
+      await createGraphics(CONFIG, DATA);
+      const FILE_EXISTS = fs.existsSync(CONFIG.outputFolderGraphics);
+      expect(FILE_EXISTS).toBeTruthy();
+      await trash(CONFIG.outputFolderGraphics);
+      expect(optimizeSVGGraphics).not.toHaveBeenCalled();
+    });
   });
 });
